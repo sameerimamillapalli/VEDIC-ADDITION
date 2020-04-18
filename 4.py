f9bddde64230ecbb0b9c617708fbeacbfacf3691
sameerimamillapalli <59050832+sameerimamillapalli@users.noreply.github.com>
2020-04-18 21:47:40 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,33 @@
+n=eval(input("Enter a 10*0 no"))
+n1=n%10
+print(n1)
+n2=n//10
+print(n2)
+a=eval(input("Enter a 10*1 no"))
+a1=a+n2
+print(a1)
+a2=a1%10
+print(a2)
+a3=a1//10
+print(a3)
+b=eval(input("Enter a 10*2 no"))
+b1=b+a3
+print(b1)
+b2=b1%10
+print(b2)
+b3=b1//10
+print(b3)
+c=eval(input("Enter a 10*3 no"))
+c1=c+b3
+print(c1)
+c2=c1%10
+print(c2)
+c3=c1//10
+print(c3)
+d=eval(input("Enter a 10*4 no"))
+d1=d+c3
+print(d1)
+d2=d1%10
+print(d2)
+d3=d1//10
+print(d3)
